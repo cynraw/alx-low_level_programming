ad66942b3ad411b5bb2fffa134a95f2949876447
cynraw <cynraw@gmail>
2023-11-21 16:57:56 -0800

    4. Free list

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * free_listint - frees a listint
+ *
+ * @head: the head of the list
+ *
+ * Return: NULL if error
+ */
+
+void free_listint(listint_t *head)
+{
+	listint_t *next;
+
+	while(head != NULL)
+ 	{
+		next = head->next;
+		free(head);
+			head = next;
+	}
+}
